f9500aeceeac4562696a3427f00a4444d5770ece
thekodu <praveenrao116@gmail.com>
2022-11-28 23:25:35 +0530

    tried different ways to handle the exception.

---
+++ src/main/java/io/exception/JavaException06.java
@@ -0,0 +1,13 @@
+package io.exception;
+
+public class JavaException06 {
+	public static void main(String[] args) {
+		try {
+			int data1 = 50/0; //may throw exception
+		} catch(ArithmeticException e) {
+			//generating exception in catch block
+			int data1 = 50/0;
+		}
+		System.out.println("rest of the code.");
+	}
+}
