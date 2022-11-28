da631e387476ea94fc9997a6f7aac60e097c5da2
thekodu <praveenrao116@gmail.com>
2022-11-28 23:01:22 +0530

    checking different ways to define try-catch

---
+++ src/main/java/io/exception/JavaException02.java
@@ -0,0 +1,12 @@
+package io.exception;
+
+public class JavaException02 {
+	public static void main(String[] args) {
+		try {
+			int data = 50/0;
+			System.out.println("rest of the code");
+		} catch (ArithmeticException e) {
+			System.out.println(e);
+		}
+	}
+}
---
+++ src/main/java/io/exception/JavaException03.java
@@ -0,0 +1,12 @@
+package io.exception;
+
+public class JavaException03 {
+	public static void main(String[] args) {
+		try {
+			int data = 50/0;
+		}catch(ArithmeticException e) {
+			System.out.println(e);
+		}
+		System.out.println("rest of the code");
+	}
+}
